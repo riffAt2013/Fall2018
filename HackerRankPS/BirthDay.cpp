4968fceac72d3de0864952c9d7c3a6069738b998
riffAt2013 <rifatmasud0@gmail.com>
2018-09-21 11:28:22 +0600

    added PS

---
+++ HackerRankPS/BirthDay.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>         
+#include <algorithm>
+
+using namespace std;
+
+int main ()
+{
+    int n; cin>>n;
+    int arr[n];
+    
+    for (int i=0; i<n; i++)
+        cin>>arr[i];
+    sort (arr, arr+n, greater<int>());
+    int max = arr[0];
+    int count = 0;
+    for (int i: arr)
+        if (i == max) count++;
+    cout<<count;
+}
+		
